18d37ff8c2ee59487dbc6cc7cb068671f60d1812
Anders Solberg <ssredna@gmail.com>
2024-01-19 21:17:08 +0100

    add a layout with a back-button on the reviews

---
+++ src/routes/[slug]/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<div class="flex max-w-lg flex-col place-items-center">
+	<Button variant="link" href="/" class="place-self-start p-0">Tilbake</Button>
+	<slot />
+</div>
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -11,18 +11,16 @@
 <h1 class="scroll-m-20 pb-8 text-4xl font-extrabold tracking-tight lg:text-5xl">
 	{data.review.review_title}
 </h1>
-<div class="max-w-lg">
-	<PortableText value={data.review.review} />
-</div>
-<footer class="flex w-full max-w-lg justify-end pt-4">
-	{#if data.review.authors}
-		<p class="italic">
-			{#each data.review.authors as author}
-				<Avatar.Root>
-					<Avatar.Image src={urlFor(author.image).width(64).height(64).url()}></Avatar.Image>
-					<Avatar.Fallback>{author.name}</Avatar.Fallback>
-				</Avatar.Root>
-			{/each}
-		</p>
-	{/if}
-</footer>
+
+<PortableText value={data.review.review} />
+
+{#if data.review.authors}
+	<footer class="flex w-full justify-end pt-4">
+		{#each data.review.authors as author}
+			<Avatar.Root>
+				<Avatar.Image src={urlFor(author.image).width(64).height(64).url()}></Avatar.Image>
+				<Avatar.Fallback>{author.name}</Avatar.Fallback>
+			</Avatar.Root>
+		{/each}
+	</footer>
+{/if}
