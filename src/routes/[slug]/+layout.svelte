8965c19fe5e10d108dc679ebb6683401ad67c82b
Anders Solberg <ssredna@gmail.com>
2024-01-24 19:45:36 +0100

    improve overall layout of review page

--- src/routes/[slug]/+layout.svelte
+++ src/routes/[slug]/+layout.svelte
@@ -2,7 +2,8 @@
 	import { Button } from '$lib/components/ui/button';
 </script>
 
-<div class="flex max-w-lg flex-col place-items-center">
+<div class="grid max-w-screen-lg">
 	<Button variant="link" href="/" class="place-self-start p-0">Tilbake</Button>
 	<slot />
 </div>
+<footer class="m-20" />
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -4,23 +4,36 @@
 	import { PortableText } from '@portabletext/svelte';
 
 	export let data;
-
-	$: console.log(data);
 </script>
 
-<h1 class="scroll-m-20 pb-8 text-4xl font-extrabold tracking-tight lg:text-5xl">
-	{data.review.review_title}
-</h1>
+<img src={urlFor(data.review.thumbnail).width(2048).height(1024).url()} alt="Logo" class="w-full" />
 
-<PortableText value={data.review.review} />
+<header class="mt-8 px-6 pb-8">
+	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+		{data.review.review_title}
+	</h1>
+	<small>Film: {data.review.movie_title}</small>
+</header>
 
-{#if data.review.authors}
-	<footer class="flex w-full justify-end pt-4">
-		{#each data.review.authors as author}
-			<Avatar.Root>
-				<Avatar.Image src={urlFor(author.image).width(64).height(64).url()}></Avatar.Image>
-				<Avatar.Fallback>{author.name}</Avatar.Fallback>
-			</Avatar.Root>
-		{/each}
-	</footer>
-{/if}
+<div class="grid grid-cols-3 gap-8 px-6">
+	<div class="col-span-2">
+		<PortableText value={data.review.review} />
+	</div>
+
+	{#if data.review.authors}
+		<div class="px-6">
+			<p class="text-sm font-light tracking-tight">Skrevet av:</p>
+			<div class="grid gap-4">
+				{#each data.review.authors as author}
+					<div class="flex w-full justify-between">
+						<p class="text-xl font-light tracking-tight">{author.name}</p>
+						<Avatar.Root class="size-16">
+							<Avatar.Image src={urlFor(author.image).width(128).height(128).url()}></Avatar.Image>
+							<Avatar.Fallback>{author.name}</Avatar.Fallback>
+						</Avatar.Root>
+					</div>
+				{/each}
+			</div>
+		</div>
+	{/if}
+</div>
