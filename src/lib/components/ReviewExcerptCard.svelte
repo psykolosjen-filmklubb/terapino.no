f74b607fa23ff2039bbea1219822c2916c9f109e
Anders Solberg <ssredna@gmail.com>
2024-01-31 11:02:34 +0100

    extract ReviewExcerptCard into its own component

---
+++ src/lib/components/ReviewExcerptCard.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import type { ReviewExcerpt } from '$lib/types';
+	import { urlFor } from '$lib/utils/image';
+
+	export let review: ReviewExcerpt;
+</script>
+
+<div class="max-w-lg">
+	<Button
+		variant="link"
+		href="/{review.slug.current}"
+		class="mb-2 h-auto scroll-m-20 flex-col items-baseline whitespace-normal p-0 text-xl font-bold tracking-tight lg:text-2xl"
+	>
+		<img src={urlFor(review.thumbnail).width(512).height(256).url()} alt="thumbnail" />
+		{review.review_title}
+	</Button>
+	<small class="text-sm leading-none lg:font-light">{review.excerpt}</small>
+</div>
--- src/lib/components/ReviewsCarousel.svelte
+++ src/lib/components/ReviewsCarousel.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button';
 	import type { ReviewExcerpt } from '$lib/types';
-	import { urlFor } from '$lib/utils/image';
 	import * as Carousel from '$lib/components/ui/carousel';
+	import ReviewExcerptCard from './ReviewExcerptCard.svelte';
 
 	export let reviews: ReviewExcerpt[];
 </script>
@@ -12,17 +11,7 @@
 		<Carousel.Content>
 			{#each reviews as review}
 				<Carousel.Item class="lg:basis-1/3">
-					<div class="max-w-lg">
-						<Button
-							variant="link"
-							href="/{review.slug.current}"
-							class="mb-2 h-auto scroll-m-20 flex-col items-baseline whitespace-normal p-0 text-xl font-bold tracking-tight lg:text-2xl"
-						>
-							<img src={urlFor(review.thumbnail).width(512).height(256).url()} alt="thumbnail" />
-							{review.review_title}
-						</Button>
-						<small class="text-sm leading-none lg:font-light">{review.excerpt}</small>
-					</div>
+					<ReviewExcerptCard {review} />
 				</Carousel.Item>
 			{/each}
 		</Carousel.Content>
