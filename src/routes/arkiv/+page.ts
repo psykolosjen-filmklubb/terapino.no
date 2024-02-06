16ebc5541b3880b3c78753dfb1413bc9f448c5fd
Anders Solberg <ssredna@gmail.com>
2024-02-06 19:41:27 +0100

    mock more of the movie archive

--- src/routes/arkiv/+page.ts
+++ src/routes/arkiv/+page.ts
@@ -51,7 +51,7 @@ const archive: Archive = [
 		id: 'third',
 		year: 2022,
 		season: 'vår',
-		color: '#00c9dd',
+		color: '#00de8f',
 		movies: [
 			{
 				title: 'Climax',
@@ -82,6 +82,140 @@ const archive: Archive = [
 				title: 'Shrek 2',
 				year: 2014,
 				director: 'Andrew Adamson, Kelly Asbury, Conrad Vernon'
+			}
+		]
+	},
+	{
+		id: 'fourth',
+		year: 2022,
+		season: 'høst',
+		color: '#006fea',
+		movies: [
+			{
+				title: 'Dazed and Confused',
+				year: 1993,
+				director: 'Richard Linklater'
+			},
+			{
+				title: 'Syk pike',
+				year: 2022,
+				director: 'Kristoffer Borgli'
+			},
+			{
+				title: 'Before Sunrise',
+				year: 1995,
+				director: 'Richard Linklater'
+			},
+			{
+				title: 'Fantastic Mr. Fox',
+				year: 2009,
+				director: 'Wes Anderson'
+			},
+			{
+				title: 'SKAM sesong 3',
+				year: 2016,
+				director: 'Julie Andem'
+			},
+			{
+				title: 'Apicalypse Now',
+				year: 1979,
+				director: 'Francis Ford Coppola'
+			},
+			{
+				title: 'Solan og Ludvig - Jul i Flåklypa',
+				year: 2013,
+				director: 'Rasmus A. Sivertsen'
+			},
+			{
+				title: 'Når nettene blir lange',
+				year: 2000,
+				director: 'Mona J. Hoel'
+			}
+		]
+	},
+	{
+		id: 'fifth',
+		year: 2023,
+		season: 'vår',
+		color: '#ff00d1',
+		movies: [
+			{
+				title: 'The Handmaiden',
+				year: 2016,
+				director: 'Park Chan-wook'
+			},
+			{
+				title: 'The Babadook',
+				year: 2014,
+				director: 'Jennifer Kent'
+			},
+			{
+				title: 'Stigma',
+				year: 2022,
+				director: 'Marius Mellem'
+			},
+			{
+				title: 'Persona',
+				year: 1966,
+				director: 'Ingmar Bergman'
+			},
+			{
+				title: 'Perfect Blue',
+				year: 1997,
+				director: 'Satoshi Kon'
+			},
+			{
+				title: 'The Conformist',
+				year: 1970,
+				director: 'Bernardo Bertolucci'
+			},
+			{
+				title: 'Shame',
+				year: 2011,
+				director: 'Steve McQueen'
+			},
+			{
+				title: 'Kill Bill: Vol. 1',
+				year: 2003,
+				director: 'Quentin Tarantino'
+			},
+			{
+				title: 'Kill Bill: Vol. 2',
+				year: 2004,
+				director: 'Quentin Tarantino'
+			}
+		]
+	},
+	{
+		id: 'sixth',
+		year: 2023,
+		season: 'høst',
+		color: '#f48c00',
+		movies: [
+			{
+				title: 'Parasite',
+				year: 2019,
+				director: 'Bong Joon-ho'
+			},
+			{
+				title: 'City of God',
+				year: 2002,
+				director: 'Fernando Meirelles, Kátia Lund'
+			},
+			{
+				title: 'Hereditary',
+				year: 2018,
+				director: 'Ari Aster'
+			},
+			{
+				title: 'Love According to Dalva',
+				year: 2022,
+				director: 'Emmanuelle Nicot'
+			},
+			{
+				title: 'Three Colours: Red',
+				year: 1994,
+				director: 'Krzysztof Kieślowski'
 			}
 		]
 	}
