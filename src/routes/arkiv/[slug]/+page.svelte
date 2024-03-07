0a3721e1f70653a5679e41b59a8f10693b5e6e64
Anders Solberg <ssredna@gmail.com>
2024-03-07 17:12:46 +0100

    fix datetimeformat on screening date in archive

--- src/routes/arkiv/[slug]/+page.svelte
+++ src/routes/arkiv/[slug]/+page.svelte
@@ -4,10 +4,23 @@
 
 	export let data;
 
+	let dateFormatter = Intl.DateTimeFormat('no', {
+		weekday: 'long',
+		month: 'long',
+		day: 'numeric',
+		year: 'numeric'
+	});
+
 	$: screening = data.screening;
 </script>
 
-<div>
+<p class="text-l mt-2 text-muted-foreground">Psykolosjen Filmklubb viste</p>
+<h1 class="scroll-m-20 text-center text-3xl font-extrabold tracking-tight lg:text-4xl">
+	{data.screening.movie_title}
+</h1>
+<p class="text-l text-muted-foreground">{dateFormatter.format(new Date(screening.date))}</p>
+
+<div class="pt-6">
 	{#if screening.poster}
 		<PosterImage poster={screening.poster} posterBlurhash={screening.posterBlurhash} />
 	{/if}
@@ -17,9 +30,3 @@
 		</div>
 	{/if}
 </div>
-
-<p class="text-l mt-2 text-muted-foreground">Psykolosjen Filmklubb viste</p>
-<h1 class="scroll-m-20 text-center text-3xl font-extrabold tracking-tight lg:text-4xl">
-	{data.screening.movie_title}
-</h1>
-<p class="text-l text-muted-foreground">{screening.date}</p>
