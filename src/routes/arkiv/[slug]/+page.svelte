80bfe30a2487543c6f3349b7778509615f207b70
Anders Solberg <ssredna@gmail.com>
2024-03-09 14:16:22 +0100

    make archive screening page a bit responsive

--- src/routes/arkiv/[slug]/+page.svelte
+++ src/routes/arkiv/[slug]/+page.svelte
@@ -14,18 +14,22 @@
 	$: screening = data.screening;
 </script>
 
-<p class="text-l mt-2 text-muted-foreground">Psykolosjen Filmklubb viste</p>
-<h1 class="scroll-m-20 text-center text-3xl font-extrabold tracking-tight lg:text-4xl">
-	{data.screening.movie_title}
-</h1>
-<p class="text-l text-muted-foreground">{dateFormatter.format(new Date(screening.date))}</p>
+<div class="grid lg:grid-cols-2">
+	<div class="mb-4 flex flex-col items-center text-center lg:mt-10">
+		<p class="text-l text-muted-foreground">Psykolosjen Filmklubb viste</p>
+		<h1 class="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl">
+			{data.screening.movie_title}
+		</h1>
+		<p class="text-l text-muted-foreground">{dateFormatter.format(new Date(screening.date))}</p>
+	</div>
 
-<div class="pt-6">
 	{#if screening.poster}
-		<PosterImage poster={screening.poster} posterBlurhash={screening.posterBlurhash} />
+		<div class="lg:order-first lg:row-span-3">
+			<PosterImage poster={screening.poster} posterBlurhash={screening.posterBlurhash} />
+		</div>
 	{/if}
 	{#if screening.posterArtists}
-		<div class="p-2">
+		<div class="p-2 lg:col-start-2 lg:row-start-3 lg:mb-4">
 			<AuthorList authors={screening.posterArtists} heading="Plakat av:" />
 		</div>
 	{/if}
