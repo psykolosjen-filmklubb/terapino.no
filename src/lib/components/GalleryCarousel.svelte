708b4af314de8419acfd66be520851235d288e59
Anders Solberg <ssredna@gmail.com>
2024-03-24 13:25:57 +0100

    add a fullscreen icon to gallery images

--- src/lib/components/GalleryCarousel.svelte
+++ src/lib/components/GalleryCarousel.svelte
@@ -6,6 +6,7 @@
 	import { urlFor } from '$lib/sanity/image';
 	import * as Carousel from '$lib/components/ui/carousel';
 	import type { CarouselAPI } from './ui/carousel/context';
+	import { Fullscreen } from 'lucide-svelte';
 
 	export let galleryID: string;
 	export let images: GalleryImage[];
@@ -31,7 +32,7 @@
 	<Carousel.Root bind:api={carouselApi}>
 		<Carousel.Content class="max-w-64">
 			{#each images as image}
-				<Carousel.Item class="flex justify-center">
+				<Carousel.Item class="relative flex justify-center">
 					<a
 						href={urlFor(image.asset).auto('format').url()}
 						data-pswp-width={image.dimensions.width}
@@ -43,9 +44,10 @@
 						<img
 							src={urlFor(image.asset).width(512).auto('format').url()}
 							alt={image.alt}
-							class="h-64 w-64 object-cover"
+							class="size-64 object-cover"
 						/>
 					</a>
+					<Fullscreen class="pointer-events-none absolute bottom-2 right-2 opacity-50" />
 				</Carousel.Item>
 			{/each}
 		</Carousel.Content>
