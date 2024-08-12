822e4db9ddfef12671867e5d50f3a7a88638bc0d
Anders Solberg <ssredna@gmail.com>
2024-08-12 18:30:44 +0200

    add bli-med page with google form

---
+++ src/routes/bli-med/+page.svelte
@@ -0,0 +1,7 @@
+<iframe
+	title="Skjema for å søke filmklubben"
+	src="https://docs.google.com/forms/d/e/1FAIpQLSf2NI7pOwOD7ipmNvUVhRWMk0IvDGvCNm739t8ju_2WcaCNVw/viewform?embedded=true"
+	class="h-[190rem] w-full border-0 lg:h-[160rem] lg:max-w-2xl"
+>
+	Laster inn…
+</iframe>
