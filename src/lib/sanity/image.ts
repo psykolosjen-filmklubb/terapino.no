20a0512dfcdf9b67c0998b28c1bca62f8623f394
Anders Lensol <ssredna@gmail.com>
2026-09-10 10:16:58 +0200

    upgrade sanity packages

--- src/lib/sanity/image.ts
+++ src/lib/sanity/image.ts
@@ -1,8 +1,8 @@
-import imageUrlBuilder from "@sanity/image-url";
+import { createImageUrlBuilder } from "@sanity/image-url";
 import { sanityClient } from "./client";
 import type { Image } from "@sanity/types";
 
-const builder = imageUrlBuilder(sanityClient);
+const builder = createImageUrlBuilder(sanityClient);
 
 export function urlFor(source: Image) {
 	return builder.image(source);
